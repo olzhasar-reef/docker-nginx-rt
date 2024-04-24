75fc4e9cab09b341fe3cb0d98c896b09db1ff363
Olzhas Arystanov <olzhas.arystanov@reef.pl>
2024-04-24 23:08:45 +0500

    Move crond start to a separate entrypoint script

--- entrypoint/50-letsencrypt-renew.sh
+++ entrypoint/50-letsencrypt-renew.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
+set -u
 
 # Run certbot renew in the background on startup
 ./scripts/run_certbot &
-
-# Start the cron daemon
-crond
---
+++ entrypoint/99-crond.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -u
+
+# Start the cron daemon
+crond
